70552a970bc32e005aa49113234c8697e7a72e8d
akshatt-15 <113547720+akshatt-15@users.noreply.github.com>
2024-10-24 02:57:29 +0530

    Create 1873C target practice.cpp

---
+++ 1873C target practice.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int arr[10][10]={{1,1,1,1,1,1,1,1,1,1},
+        {1,2,2,2,2,2,2,2,2,1},
+        {1,2,3,3,3,3,3,3,2,1},
+        {1,2,3,4,4,4,4,3,2,1},
+        {1,2,3,4,5,5,4,3,2,1},
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,2,2,2,2,2,2,2,2,1},
+        {1,2,3,3,3,3,3,3,2,1},
+        {1,2,3,4,4,4,4,3,2,1},
+        {1,2,3,4,5,5,4,3,2,1}};
+        long long ans=0;
+        char ch;
+        for(int i=0;i<10;i++)
+        {
+            for(int j=0;j<10;j++)
+            {
+                cin>>ch;
+                if(ch=='X')
+                {
+                    ans+=arr[i][j];
+                }
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
