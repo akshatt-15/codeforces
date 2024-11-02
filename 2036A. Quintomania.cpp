bc19a17a1abf51546530f94e9989d796c3a4a8d2
akshatt-15 <113547720+akshatt-15@users.noreply.github.com>
2024-11-02 23:07:36 +0530

    Create 2036A. Quintomania.cpp

---
+++ 2036A. Quintomania.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+     int t;
+    cin >> t;
+    
+    
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        vector<int> notes(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> notes[i];
+        }
+        
+        bool isPerfect = true;
+        
+    
+        for (int i = 1; i < n; ++i) {
+            int interval = abs(notes[i] - notes[i - 1]);
+            if (interval != 5 && interval != 7) {
+                isPerfect = false;
+                break;
+            }
+        }
+        
+        
+        if (isPerfect) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    
+    return 0;
+}
