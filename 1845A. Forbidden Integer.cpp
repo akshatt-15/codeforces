e617c5d316550d1bb0e67b3c0de39645e6489046
akshatt-15 <113547720+akshatt-15@users.noreply.github.com>
2024-11-13 20:21:55 +0530

    Create 1845A. Forbidden Integer.cpp

---
+++ 1845A. Forbidden Integer.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    
+     int tc;
+     cin>>tc;
+     while(tc--){
+        int n,k,x;
+        cin>>n>>k>>x;
+        if(n%2!=0){
+            if(x!=1){
+                cout<<"YES"<<endl<<n<<endl;
+                while(n-->0) cout<<1<<" ";
+            }
+            else if(k>2){
+                cout<<"YES"<<endl;
+                int m = (n-3)/2+1;
+                cout<<m<<endl;
+                cout<<3<<" ";
+                for(int i=1;i<=m-1;i++) cout<<2<<" ";
+            }
+            else cout<<"NO"<<endl;
+        }
+        else{
+            if(x!=1){
+                cout<<"YES"<<endl<<n<<endl;
+                while(n-->0) cout<<1<<" ";
+            }
+            else if(x!=2 and k>=2){
+                cout<<"YES"<<endl;
+                cout<<n/2<<endl;
+                for(int i=1;i<=n/2;i++) cout<<2<<" ";
+            }
+            else cout<<"NO"<<endl;
+        }
+        cout<<endl;
+    }
+        
+}
