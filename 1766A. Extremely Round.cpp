c895df3669fdfca360b7526eaf0eb9e69171de89
akshatt-15 <113547720+akshatt-15@users.noreply.github.com>
2024-11-17 02:39:01 +0530

    1766A. Extremely Round.cpp

---
+++ 1766A. Extremely Round.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+     int tc;
+     cin>>tc;
+     while(tc--){
+        int n; cin>>n;
+        int digits = 0, ld;
+        while(n>0){
+            ld = n%10;
+            n/=10;
+            digits++;
+        }
+        cout<<(ld+(digits-1)*9);
+        cout<<endl;
+    }    
+}
